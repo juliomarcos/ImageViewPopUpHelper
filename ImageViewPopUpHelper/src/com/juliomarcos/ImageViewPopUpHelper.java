350f6c01a58c465212db6f86ce28099b034f38ea
Ivan Foong <ivan.foong@docomointertouch.com>
2014-10-30 11:10:33 +0800

    corrected indentation

--- ImageViewPopUpHelper/src/com/juliomarcos/ImageViewPopUpHelper.java
+++ ImageViewPopUpHelper/src/com/juliomarcos/ImageViewPopUpHelper.java
@@ -16,7 +16,7 @@ import android.widget.ImageView;
 public class ImageViewPopUpHelper {
 
 	private Activity context;
-	
+
 	private Drawable imageViewDrawable;
 	private int finalImageWidth;
 	private int finalImageHeight;
@@ -30,11 +30,11 @@ public class ImageViewPopUpHelper {
 		imageViewDrawable = drawable;
 		int imageRealWidth = imageViewDrawable.getIntrinsicWidth();
 		int imageRealHeight = imageViewDrawable.getIntrinsicHeight();
-		
-		Point screenDimensions = getScreenDimensions(context);		
+
+		Point screenDimensions = getScreenDimensions(context);
 		final int screenWidth = screenDimensions.x;
 		final int screenHeight = screenDimensions.y;
-				
+
 		// Algoritmo iterativo para achar um tamanho final para a imagem
 		while(shouldScaleDown && (imageRealWidth >= screenWidth || imageRealHeight >= screenHeight)) {
 			imageRealWidth  *= 0.9;
@@ -49,12 +49,12 @@ public class ImageViewPopUpHelper {
     }
 
 		finalImageWidth = imageRealWidth;
-		finalImageHeight = imageRealHeight;		
-		
+		finalImageHeight = imageRealHeight;
+
 		if (requireResizingOfBitmap) {
 			Bitmap bitmap = drawableToBitmap(imageViewDrawable);
 			BitmapDrawable resizedBitmapDrawable = new BitmapDrawable(
-					context.getResources(), 
+					context.getResources(),
 					Bitmap.createScaledBitmap(bitmap, finalImageWidth, finalImageHeight, false));
 			poppedImageView.setBackgroundDrawable(resizedBitmapDrawable);
 		}
@@ -81,14 +81,14 @@ public class ImageViewPopUpHelper {
 
 		this.context = context;
 		poppedImageView = new ImageView(context);
-		
+
 		dialog = new Dialog(context);
 		dialog.requestWindowFeature((int) Window.FEATURE_NO_TITLE);
 		dialog.setContentView(poppedImageView);
-		dialog.getWindow().setBackgroundDrawable(null); // Without this line there is a very small border around the image (1px)				
+		dialog.getWindow().setBackgroundDrawable(null); // Without this line there is a very small border around the image (1px)
 		dialog.setCanceledOnTouchOutside(true); // Gingerbread support
-		
-		imageView.setOnClickListener(new View.OnClickListener() {						
+
+		imageView.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 
@@ -119,7 +119,7 @@ public class ImageViewPopUpHelper {
 		// Get screen size
 		Display display = context.getWindowManager().getDefaultDisplay();
 		Point size = new Point();
-		
+
 		int currentapiVersion = android.os.Build.VERSION.SDK_INT;
 		if (currentapiVersion >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR1){
 		    display.getRealSize(size);
@@ -127,23 +127,23 @@ public class ImageViewPopUpHelper {
 		else if (currentapiVersion >= android.os.Build.VERSION_CODES.HONEYCOMB_MR2){
 			display.getSize(size);
 		} else{
-		    size.x = display.getWidth(); 
+		    size.x = display.getWidth();
 		    size.y = display.getHeight();
 		}
 		return size;
 	}
-	
+
 	private Bitmap drawableToBitmap (Drawable drawable) {
 	    if (drawable instanceof BitmapDrawable) {
 	        return ((BitmapDrawable)drawable).getBitmap();
 	    }
 
 	    Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Config.ARGB_8888);
-	    Canvas canvas = new Canvas(bitmap); 
+	    Canvas canvas = new Canvas(bitmap);
 	    drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
 	    drawable.draw(canvas);
 
 	    return bitmap;
 	}
-	
+
 }
